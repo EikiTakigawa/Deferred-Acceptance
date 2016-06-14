d63f95ad70d8eded24fcf0cd740a628f7555b926
EikiTakigawa <E.Shine@outlook.jp>
2016-06-14 13:33:07 +0900

    stableな結果が得られるように大幅に改めました

--- DA_1to1.jl
+++ DA_1to1.jl
@@ -1,32 +1,39 @@
 
-function Match(m, n, m_prefs, n_prefs)
-    m_matched = zeros(m)
-    n_matched = zeros(n)
-    i = 1
-    for i in 1:n+1
-        k = 1
-        for k in 1:m
-            if m_prefs[i, k] == 0
-                m_prefs[i:end, k] = 0
-            else
-                if m_matched[k] == 0
-                    if n_matched[m_prefs[i, k]] == 0
-                        if findfirst(n_prefs[:, m_prefs[i,k]] , k) < findfirst(n_prefs[:, m_prefs[i,k]], 0)
-                            n_matched[m_prefs[i, k]] = k
-                            m_matched[k] = m_prefs[i, k]
+function Match(m_prefs, n_prefs)
+    m = size(m_prefs,2)
+    n = size(n_prefs,2)
+    m_matched = Array(Int,m)
+    for i in 1:m
+        m_matched[i] = -1
+    end
+    n_matched = zeros(Int,n)
+    while true
+        for j in 1:m
+            k = 1
+            while m_matched[j] == -1
+                if m_prefs[k, j] == 0
+                    m_matched[j] = 0
+                else
+                    if n_matched[m_prefs[k, j]] == 0
+                        if findfirst(n_prefs[:, m_prefs[k,j]] , j) < findfirst(n_prefs[:, m_prefs[k,j]], 0)
+                            n_matched[m_prefs[k, j]] = j
+                            m_matched[j] = m_prefs[k, j]
                         end
                     else
-                        if findfirst(n_prefs[:, m_prefs[i,k]], k) < findfirst(n_prefs[: ,m_prefs[i,k]], n_matched[m_prefs[i,k]])
-                            m_matched[n_matched[m_prefs[i,k]]] = 0
-                            n_matched[m_prefs[i,k]] = k
-                            m_matched[k] = m_prefs[i,k]
+                        if findfirst(n_prefs[:, m_prefs[k,j]], j) < findfirst(n_prefs[: ,m_prefs[k,j]], n_matched[m_prefs[k,j]])
+                            m_matched[n_matched[m_prefs[k,j]]] = -1
+                            n_matched[m_prefs[k,j]] = j
+                            m_matched[j] = m_prefs[k,j]
                         end
                     end
                 end
+                k += 1
             end
-            k += 1
         end
-        i += 1
+        check_matched = m_matched[m_matched.!= -1]
+        if m_matched == check_matched
+            break
+        end
     end
-    return m_matched, n_matched
+    return m_matched,n_matched
 end
